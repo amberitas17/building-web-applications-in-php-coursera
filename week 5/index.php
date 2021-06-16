9a16194cc4d449829c095160bdf8ebd514a79b00
amberitas17 <67531380+amberitas17@users.noreply.github.com>
2021-06-16 17:38:02 +0800

    week 5 assignment

---
+++ week 5/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang = "en">
+<head>
+<title>Amber Cubacub's PHP</title>
+</head>
+<body>
+<h1> Amber Cubacub's</h1>
+<p>The SHA256 hash of "Amber Cubacub" is <?php print hash('sha256','Amber Cubacub'); ?></p>
+<pre> ASCII ART:
+           ***
+          *****
+         **   **
+        **     **
+       ***********
+      *************
+     **           **
+    **             **
+</pre>
+<a href = "check.php"> Click here to check the error setting </a>
+<br/>
+<a href = "fail.php"> Click here to cause a traceback </a>
+</body>
+</html>
